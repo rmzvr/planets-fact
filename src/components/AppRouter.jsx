664adab1f993dbcbdbafc2369a40430827b44b59
rzvr <work.folder.d@protonmail.com>
2022-03-12 20:04:43 +0200

    Added more planets

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
+
 import Mercury from "../pages/Mercury";
+import Venus from "../pages/Venus";
+import Earth from "../pages/Earth";
+import Mars from "../pages/Mars";
+import Jupiter from "../pages/Jupiter";
+import Saturn from "../pages/Saturn";
+import Uranus from "../pages/Uranus";
+import Neptune from "../pages/Neptune";
 
 function AppRouter() {
   return (
     <Routes>
-      {/* <Route path="*" element={<Navigate to="/mercury" />} /> */}
+      <Route path="*" element={<Navigate to="/mercury" />} />
       <Route path="/" element={<Navigate to="mercury" />} />
       <Route path="mercury" element={<Mercury />} />
+      <Route path="venus" element={<Venus />} />
+      <Route path="earth" element={<Earth />} />
+      <Route path="mars" element={<Mars />} />
+      <Route path="jupiter" element={<Jupiter />} />
+      <Route path="saturn" element={<Saturn />} />
+      <Route path="uranus" element={<Uranus />} />
+      <Route path="neptune" element={<Neptune />} />
     </Routes>
   );
 }
---
+++ src/pages/Jupiter.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState } from "react";
+import styles from "../components/App/App.module.scss";
+import planets from "../data.json";
+import useImage from "../utils/useImage";
+import getPlanet from "../utils/getPlanet";
+import Source from "../components/UI/Source/Source";
+import TabList from "../components/UI/Tab/TabList";
+import CardList from "../components/UI/Card/CardList";
+
+function Jupiter() {
+  const [activeTab, setActiveTab] = useState("overview");
+  const planet = getPlanet(planets);
+  const { image } = useImage(planet, activeTab);
+
+  return (
+    <div className="container">
+      <main className={styles.main}>
+        <img className={styles.image} src={image} alt={planet.name} />
+        <div className={styles.about}>
+          <h1 className={styles.title}>{planet.name}</h1>
+          <p className={styles.content}>{planet[activeTab].content}</p>
+          <Source planet={planet} activeTab={activeTab} />
+        </div>
+        <TabList activeTab={activeTab} setActiveTab={setActiveTab} />
+        <CardList planet={planet} />
+      </main>
+    </div>
+  );
+}
+
+export default Jupiter;
