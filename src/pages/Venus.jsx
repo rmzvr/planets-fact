3f0ca77632ed58ff6488ef6957ae74d438401604
rzvr <work.folder.d@protonmail.com>
2022-03-15 21:36:32 +0200

    Disabled eslint

--- src/pages/Venus.jsx
+++ src/pages/Venus.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable react-hooks/exhaustive-deps */
+
 import React, { useState } from "react";
 import styles from "../components/App/App.module.scss";
 import planets from "../planets.json";
